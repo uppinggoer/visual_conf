19ba8587eaa59ff7003845b3a9c15e3427c8102c
杨宏志 <yanghongzhi@bytedance.com>
2020-02-20 13:12:11 +0800

    T

---
+++ keys/.example.js
@@ -0,0 +1,41 @@
+var data = {
+ "label": {fontSize: 30},
+ "symbolSize": 30,
+ "name": "",
+ "children": [
+  {
+   "label": {fontSize: 15, rotate: 0, align: 'center'},
+   "symbolSize": 30,
+   "name": "a==1",
+   "children": [
+    {
+     "label": {fontSize: 15},
+     "symbolSize": 30,
+     "name": "cluster",
+     "children": [
+      {"name": "AgglomerativeCluster", "value": 3938, "symbol":'rect'},
+      {"name": "CommunityStructure", "value": 3812},
+      {"name": "HierarchicalCluster", "value": 6714},
+      {"name": "MergeEdge", "value": 743}
+     ]
+    },
+    {
+     "name": "graph",
+     "children": [
+      {"name": "BetweennessCentrality", "value": 3534},
+      {"name": "LinkDistance", "value": 5731},
+      {"name": "MaxFlowMinCut", "value": 7840},
+      {"name": "ShortestPaths", "value": 5914},
+      {"name": "SpanningTree", "value": 3416}
+     ]
+    },
+    {
+     "name": "optimization",
+     "children": [
+      {"name": "AspectRatioBanker", "value": 7074}
+     ]
+    }
+   ]
+  }
+ ]
+}
